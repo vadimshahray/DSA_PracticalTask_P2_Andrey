dd96bd073962268cb8f62d3daf047cc9c98c5e7f
Вадим Шахрай <vadimshahray.workmail@gmail.com>
2022-04-03 00:08:07 +0700

    Очередь изменена на очередь с приоритетом.

--- DSA_PracticalTask_P2_Andrey/DSA_PracticalTask_P2_Andrey.cpp
+++ DSA_PracticalTask_P2_Andrey/DSA_PracticalTask_P2_Andrey.cpp
@@ -2,53 +2,71 @@
 #include <stdio.h>
 #include <windows.h>
 
-const UCHAR N = 4; // Максимальное кол-во элементов в деке
+const UCHAR N = 7; // Максимальное кол-во элементов в очереди
 
 enum COMMAND // Команды консольного меню
 {
-   PUSH,    // Добавить элемент в очередь
-   POP,     // Вытолкнуть элемент из очереди
-   EMPTY,   // Проверить, пуста ли очередь
-   FULL,    // Проверить, полна ли очередь
-   CLEAR,   // Очистить очередь
-   PRINT,   // Вывести все элементы очереди на консоль
-   EXIT     // Завершить выполнение программы
+   PUSH,     // Добавить элемент в очередь
+   POP,      // Вытолкнуть элемент из очереди
+   EMPTY,    // Проверить, пуста ли очередь
+   FULL,     // Проверить, полна ли очередь
+   CLEAR,    // Очистить очередь
+   PRINT,    // Вывести все элементы очереди на консоль
+   EXIT      // Завершить выполнение программы
 };
 
+struct item
+{
+   private:
+   int priority;
+   
+   public:
+   item(char _value = 0, int _priority = -1) : value(_value), priority(_priority) { }
+   
+   char value;
+   
+   int get_priority() { return priority; };
+};
 
 struct queue
 {
    private:
-   char beg = -1, end = -1, data[N] = { };
+   char beg = -1, end = -1;
+   item data[N] = { };
 
    public:
    /// <summary>
-   /// Добавляет элемент e в конец очереди
+   /// Добавляет элемент item в конец очереди
    /// </summary>
-   /// <param name="e">Элемент, который нужно добавить</param>
+   /// <param name="i">Элемент, который нужно добавить</param>
    /// <returns>Выполнилась ли операция</returns>
-   bool push(char e)
+   bool push(item i)
    {
       bool is_f = full(), is_e = empty();
       if (!is_f)
       {
+         char p = beg;
+         for ( ; data[p].get_priority() <= i.get_priority() && p != end + 1; p == N - 1 && p != end ? p = 0 : p++);
+         for (char j = end == N - 1 ? 0 : end; j != p - 1; j == 0 && j != p ? j = N - 1 : j--)
+            data[j + 1] = data[j];
+
+         data[p] = i;
          end = is_e ? beg = 0 : end == N - 1 ? 0 : end + 1;
-         data[end] = e;
       }
 
       return !is_f;
    }
    /// <summary>
-   /// Выталкивет элемент из начала очереди и передает его в e
+   /// Выталкивет элемент из начала очереди и передает его в item
    /// </summary>
-   /// <param name="e">Куда передать вытолкнутый элемент</param>
+   /// <param name="i">Куда передать вытолкнутый элемент</param>
    /// <returns>Выполнилась ли операция</returns>
-   bool pop(char &e)
+   bool pop(item &i)
    {
       bool is_e = empty(), will_e = beg == end;
       if (!is_e)
       {
-         e = data[beg];
+         i = data[beg];
          beg = will_e ? end = -1 : beg == N - 1 ? 0 : beg + 1;
       }
 
@@ -67,17 +85,17 @@ struct queue
    /// <summary>
    /// Очищает очередь
    /// </summary>
-   void clear() { for (char e = 0; pop(e); ); }
+   void clear() { for (item i = 0; pop(i); ); }
    /// <summary>
    /// Выводит элементы очереди на консоль
    /// </summary>
    void print()
    {
       queue d;
-      char e = 0;
+      item i = 0;
 
-      for (; pop(e); d.push(e)) printf_s("%c ", e);
-      for (; d.pop(e); push(e));
+      for ( ; pop(i); d.push(i)) printf_s("%c ", i.value);
+      for ( ; d.pop(i); push(i));
    }
 };
 
@@ -88,12 +106,22 @@ int main()
    SetConsoleCP(1251);
    SetConsoleOutputCP(1251);
 
+   item i;
    queue q;
-   char cmd, elem;
+   int p;
+   char cmd, v;
    bool repeat = true;
    do
    {
-      printf_s("Выберите команду:\n[%d] Добавить элемент в очередь;\n[%d] Вытолкнуть элемент из очереди;\n[%d] Проверить, пуста ли очередь;\n[%d] Проверить, полна ли очередь;\n[%d] Очистить очередь;\n[%d] Вывести содержимое очереди;\n[%d] Завершить работу.\nКоманда: ",  PUSH, POP, EMPTY, FULL, CLEAR, PRINT, EXIT);
+      printf_s("Выберите команду:\n"
+               "[%d] Добавить элемент в очередь;\n"
+               "[%d] Вытолкнуть элемент из очереди;\n"
+               "[%d] Проверить, пуста ли очередь;\n"
+               "[%d] Проверить, полна ли очередь;\n"
+               "[%d] Очистить очередь;\n"
+               "[%d] Вывести содержимое очереди;\n"
+               "[%d] Завершить работу.\n"
+               "Команда: ", PUSH, POP, EMPTY, FULL, CLEAR, PRINT, EXIT);
 
       scanf_s("\n%c", &cmd, 1);
       printf_s("\n");
@@ -102,12 +130,14 @@ int main()
       {
          case PUSH:
             printf_s("Введите элемент: ");
-            scanf_s("\n%c", &elem, 1);
+            scanf_s("\n%c", &v, 1);
+            printf_s("Укажите его приоритет: ");
+            scanf_s("\n%d", &p); 
 
-            printf_s(q.push(elem) ? "Элемент успешно добавлен."
-                                  : "Не удалось добавить элемент: очередь полна."); break;
+            printf_s(q.push(item(v, p)) ? "Элемент успешно добавлен."
+                                        : "Не удалось добавить элемент: очередь полна."); break;
          case POP:
-            q.pop(elem) ? printf_s("Вытолкнутый элемент: %c", elem)
+            q.pop(i) ? printf_s("Вытолкнутый элемент: %c", i.value)
                         : printf_s("Не удалось выполнить данную операцию: очередь пуста."); break;
          case EMPTY:
             printf_s(q.empty() ? "Очередь пуста." : "Очередь не пуста."); break;
